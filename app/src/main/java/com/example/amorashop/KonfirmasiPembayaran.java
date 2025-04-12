a0c22796e48537d6546304a8d7473c44bd9c04ab
M Rizki Musthofa <mrizkimustofa@gmail.com>
2025-04-13 03:16:03 +0700

    Konfirmasi Pembayaran Donee

--- app/src/main/java/com/example/amorashop/KonfirmasiPembayaran.java
+++ app/src/main/java/com/example/amorashop/KonfirmasiPembayaran.java
@@ -1,21 +1,27 @@
 package com.example.amorashop;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View; // Added import for View
 
 public class KonfirmasiPembayaran extends AppCompatActivity {
+
+    private View mainView; // Declare the main view
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up edge-to-edge layout
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_konfirmasi_pembayaran);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Set up window insets for padding
+        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
